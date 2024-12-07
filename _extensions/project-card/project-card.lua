71e9e088b19af7ea7695c301f727cfee310d7d2e
Priyam Kabra <priyamkabra7@gmail.com>
2024-12-06 20:38:11 -0500

    added basic dark mode support

--- _extensions/project-card/project-card.lua
+++ _extensions/project-card/project-card.lua
@@ -1,11 +1,11 @@
-function createcard(title, desc, img, link, ghlink)
+function createcard(title, desc, language, img, link, ghlink)
   local htmlstr = [[
-<div class="card">
+<div class="card" data-language="%s">
     <img src="%s" alt="Card Image">
     <div class="card-content">
         <h3 class="card-title">
             <span>%s</span>
-            <a href="%s" target="_blank" class="github-link" title="View on GitHub">
+            <a href="%s" target="_blank" class="text-body" title="View on GitHub">
                 <i class="bi bi-github"></i>
             </a>
         </h3>
@@ -14,7 +14,7 @@ function createcard(title, desc, img, link, ghlink)
     </div>
 </div>
 ]]
-  return string.format(htmlstr, img, title, ghlink, desc, link)
+  return string.format(htmlstr, language, img, title, ghlink, desc, link)
 end
 
 return {
@@ -22,12 +22,13 @@ return {
     -- get args
     local title = kwargs["title"]
     local desc = kwargs["desc"]
+    local language = kwargs["language"]
     local img = kwargs["img"]
     local link = kwargs["link"]
     local ghlink = kwargs["ghlink"]
     -- return html block for a card based on args
     return pandoc.RawBlock('html',
-    createcard(title, desc, img, link, ghlink)
+    createcard(title, desc, language, img, link, ghlink)
   )
   end
 }

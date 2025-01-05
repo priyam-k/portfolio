ff8a96a05f4479186dcb991b1dc045fe5b9c2264
Priyam Kabra <priyamkabra7@gmail.com>
2025-01-04 19:46:58 -0500

    updated card elements and styles, added some projects + imgs

--- _extensions/project-card/project-card.lua
+++ _extensions/project-card/project-card.lua
@@ -1,20 +1,28 @@
-function createcard(title, desc, language, img, link, ghlink)
+function createcard(title, desc, language, img, srclink)
+  local linkmode = "Source"
+  local biclass = "bi-link-45deg"
+  if string.find(srclink, "github") then
+    linkmode = "on Github"
+    biclass = "bi-github"
+  end
   local htmlstr = [[
 <div class="card" data-language="%s">
-    <img src="%s" alt="Card Image">
+    <div class="imgcontainer">
+      <img src="%s" alt="Card Image">
+      <div class="text-overlay">%s</div>
+    </div>
     <div class="card-content">
         <h3 class="card-title">
             <span>%s</span>
-            <a href="%s" target="_blank" class="text-body" title="View on GitHub">
-                <i class="bi bi-github"></i>
+            <a href="%s" target="_blank" class="text-body" title="View %s">
+                <i class="bi %s"></i>
             </a>
         </h3>
         <p class="card-description">%s</p>
-        <a href="%s" target="_blank">Learn More</a>
     </div>
 </div>
 ]]
-  return string.format(htmlstr, language, img, title, ghlink, desc, link)
+  return string.format(htmlstr, language, img, language, title, srclink, linkmode, biclass, desc)
 end
 
 return {
@@ -24,11 +32,11 @@ return {
     local desc = kwargs["desc"]
     local language = kwargs["language"]
     local img = kwargs["img"]
-    local link = kwargs["link"]
-    local ghlink = kwargs["ghlink"]
+    -- local link = kwargs["link"]
+    local srclink = kwargs["srclink"]
     -- return html block for a card based on args
     return pandoc.RawBlock('html',
-    createcard(title, desc, language, img, link, ghlink)
+    createcard(title, desc, language, img, srclink)
   )
   end
 }
